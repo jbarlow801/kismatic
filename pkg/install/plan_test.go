ef285364c4ac33d8747d7cee65545bb635288eaa
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-04-04 13:45:20 -0400

    Dockerize kismatic installer

--- pkg/install/plan_test.go
+++ pkg/install/plan_test.go
@@ -201,3 +201,11 @@ func TestReadDeprecatedDashboard(t *testing.T) {
 	}
 
 }
+
+// the test will run either in a container or locally, should always be absolute path
+func TestGetSSHKey(t *testing.T) {
+	key := getSSHKey()
+	if !filepath.IsAbs(key) {
+		t.Errorf("Expected key to be an absolute path, instead got %s", key)
+	}
+}
